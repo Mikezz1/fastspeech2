fix variance encoder forward, fix pre-layernorm

configs/local_config.py
@@ -12,15 +12,15 @@ class FastSpeechConfig:
     vocab_size = 300
     max_seq_len = 3000
 
-    encoder_dim = 256
+    encoder_dim = 128
     encoder_n_layer = 2
-    encoder_head = 4
-    encoder_conv1d_filter_size = 1024
+    encoder_head = 2
+    encoder_conv1d_filter_size = 256
 
-    decoder_dim = 256
+    decoder_dim = 128
     decoder_n_layer = 2
-    decoder_head = 4
-    decoder_conv1d_filter_size = 1024
+    decoder_head = 2
+    decoder_conv1d_filter_size = 256
 
     fft_conv1d_kernel = (9, 1)
     fft_conv1d_padding = (4, 0)
@@ -59,9 +59,9 @@ class TrainConfig:
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    batch_size = 48
-    epoch_len = -1
-    epochs = 150
+    batch_size = 4
+    epoch_len = 8
+    epochs = 20
     n_warm_up_step = 3000
 
     learning_rate = 1e-3
@@ -69,11 +69,11 @@ class TrainConfig:
     grad_clip_thresh = 1.0
     decay_step = [500000, 1000000, 2000000]
 
-    save_step = 200
-    log_step = 80
+    save_step = 4
+    log_step = 2
     clear_Time = 20
 
-    batch_expand_size = 48
+    batch_expand_size = 1
 
     hop_length = 256
     win_length = 1024
@@ -92,6 +92,6 @@ class TrainConfig:
     pitch_std = 111.120
 
     alignment_min = 0
-    alignment_max = 4.304  # log
+    alignment_max = 4.304  # log(74.000)
     alignment_mean = 5.669
     alignment_std = 4.940

fastspeech2/model/blocks.py
@@ -88,7 +88,7 @@ class MultiHeadAttention(nn.Module):
 
         self.attention = ScaledDotProductAttention(
             temperature=d_k**0.5)
-        self.layer_norm = nn.LayerNorm(d_model)
+        #self.layer_norm = nn.LayerNorm(d_model)
 
         self.fc = nn.Linear(n_head * d_v, d_model)
         nn.init.xavier_normal_(self.fc.weight)
@@ -136,7 +136,7 @@ class MultiHeadAttention(nn.Module):
             sz_b, len_q, -1)  # b x lq x (n*dv)
 
         output = self.dropout(self.fc(output))
-        output = self.layer_norm(output + residual)
+        #output = self.layer_norm(output + residual)
 
         return output, attn
 
@@ -164,11 +164,13 @@ class PositionwiseFeedForward(nn.Module):
 
     def forward(self, x):
         residual = x
+
+        output = self.layer_norm(x)
         output = x.transpose(1, 2)
         output = self.w_2(F.relu(self.w_1(output)))
         output = output.transpose(1, 2)
         output = self.dropout(output)
-        output = self.layer_norm(output + residual)
+        output = output + residual
 
         return output
 
@@ -196,6 +198,7 @@ class FFTBlock(torch.nn.Module):
     def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None):
 
         residual = enc_input
+        enc_input = self.layer_norm(enc_input)
         enc_output, enc_slf_attn = self.slf_attn(
             enc_input, enc_input, enc_input, mask=slf_attn_mask)
 
@@ -203,8 +206,7 @@ class FFTBlock(torch.nn.Module):
             enc_output *= non_pad_mask
         # layer norm + residual
 
-        enc_output = self.layer_norm(enc_output + residual)
-
+        enc_output = enc_output + residual
         enc_output = self.pos_ffn(enc_output)
 
         if non_pad_mask is not None:

fastspeech2/model/fastspeech.py
@@ -34,11 +34,15 @@ class FastSpeech(nn.Module):
         if self.training:
             output, duration_predictor_output = self.length_regulator(
                 x, alpha, length_target, mel_max_length)
-            output, energy_predictor_output = self.energy_adaptor(
+
+            pitch_embedding, pitch_predictor_output = self.pitch_adaptor(
                 output, energy_target)
 
-            output, pitch_predictor_output = self.pitch_adaptor(
+            energy_embedding, energy_predictor_output = self.energy_adaptor(
                 output, energy_target)
+
+            output = output + pitch_embedding
+            output = output + energy_embedding
 
             output = self.decoder(output, mel_pos)
             output = self.mask_tensor(output, mel_pos, mel_max_length)

fastspeech2/model/variance_adaptors.py
@@ -70,13 +70,13 @@ class EnergyAdaptor(nn.Module):
         if target is not None:
             embedding = self.energy_embedding(
                 torch.bucketize(target, self.energy_bins))
-            x = x+embedding
-            return x, energy_predictions
+            #x = x+embedding
+            return embedding, energy_predictions
         else:
             embedding = self.energy_embedding(
                 torch.bucketize(energy_predictions, self.energy_bins))
-            x += embedding
-            return x
+            #x += embedding
+            return embedding
 
 
 class PitchAdaptor(nn.Module):
@@ -107,13 +107,13 @@ class PitchAdaptor(nn.Module):
         if target is not None:
             embedding = self.pitch_embedding(
                 torch.bucketize(target, self.pitch_bins))
-            x = x+embedding
-            return x, pitch_predictions
+            #x = x+embedding
+            return embedding, pitch_predictions
         else:
             embedding = self.pitch_embedding(
                 torch.bucketize(pitch_predictions, self.pitch_bins))
-            x += embedding
-            return x
+            #x += embedding
+            return embedding
 
 
 class VarianceAdaptor(nn.Module):

train.py
@@ -13,56 +13,72 @@ from torch.optim.lr_scheduler import OneCycleLR
 from dataclasses import dataclass
 from configs.base_config import *
 import dataclasses
-
-# Init configs
-mel_config = MelSpectrogramConfig()
-model_config = FastSpeechConfig()
-train_config = TrainConfig()
-
-# Load data
-buffer = get_data_to_buffer(train_config)
-collator = collate_fn_tensor(train_config)
-dataset = BufferDataset(buffer)
-training_loader = DataLoader(
-    dataset,
-    batch_size=train_config.batch_expand_size * train_config.batch_size,
-    shuffle=True,
-    collate_fn=collator,
-    drop_last=True,
-    num_workers=0
-)
-# add validation
+import argparse
+import importlib
+text = importlib.import_module('matplotlib.text')
 
 
-# Load model
-model = FastSpeech(model_config, mel_config, train_config)
-model = model.to(train_config.device)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", default='base_config', type=str,
+                        help="config file name (without .py)"
+                        )
+
+    args = parser.parse_args()
+    return args
 
 
-# Specify other stuff
-fastspeech_loss = FastSpeechLoss(train_config)
+if __name__ == '__main__':
+    args = parse_args()
+    exec(f"from configs.{args.config} import *")
+    # Init configs
+    mel_config = MelSpectrogramConfig()
+    model_config = FastSpeechConfig()
+    train_config = TrainConfig()
 
-optimizer = torch.optim.AdamW(
-    model.parameters(),
-    lr=train_config.learning_rate,
-    betas=(0.9, 0.98),
-    eps=1e-9)
+    # Load data
+    buffer = get_data_to_buffer(train_config)
+    collator = collate_fn_tensor(train_config)
+    dataset = BufferDataset(buffer)
+    training_loader = DataLoader(
+        dataset,
+        batch_size=train_config.batch_expand_size * train_config.batch_size,
+        shuffle=True,
+        collate_fn=collator,
+        drop_last=True,
+        num_workers=0
+    )
+    # add validation
 
-scheduler = OneCycleLR(optimizer, **{
-    "steps_per_epoch": len(training_loader) * train_config.batch_expand_size,
-    "epochs": train_config.epochs,
-    "anneal_strategy": "cos",
-    "max_lr": train_config.learning_rate,
-    "pct_start": 0.1
-})
+    # Load model
+    model = FastSpeech(model_config, mel_config, train_config)
+    model = model.to(train_config.device)
 
-# wandb_config = {
-#     **dataclasses.asdict(train_config),
-#     **dataclasses.asdict(model_config),
-#     **dataclasses.asdict(mel_config)}
-logger = WanDBWriter(train_config)
-trainer = Trainer(
-    training_loader=training_loader, train_config=train_config, model=model,
-    logger=logger, scheduler=scheduler, optimizer=optimizer,
-    fastspeech_loss=fastspeech_loss)
-trainer.train()
+    # Specify other stuff
+    fastspeech_loss = FastSpeechLoss(train_config)
+
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=train_config.learning_rate,
+        betas=(0.9, 0.98),
+        eps=1e-9)
+
+    scheduler = OneCycleLR(optimizer, **{
+        "steps_per_epoch": len(training_loader) * train_config.batch_expand_size,
+        "epochs": train_config.epochs,
+        "anneal_strategy": "cos",
+        "max_lr": train_config.learning_rate,
+        "pct_start": 0.1
+    })
+
+    # wandb_config = {
+    #     **dataclasses.asdict(train_config),
+    #     **dataclasses.asdict(model_config),
+    #     **dataclasses.asdict(mel_config)}
+    logger = WanDBWriter(train_config)
+    trainer = Trainer(training_loader=training_loader,
+                      train_config=train_config, model=model, logger=logger,
+                      scheduler=scheduler, optimizer=optimizer,
+                      fastspeech_loss=fastspeech_loss)
+
+    trainer.train()