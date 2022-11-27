b12821c2de1cf8ce1e8b719da7135e392371307b
Михаил Олейник <mikhailoleynik@192.168.1.3>
2022-11-27 21:51:07 +0300

    add MFA

--- configs/base_config.py
+++ configs/base_config.py
@@ -50,8 +50,8 @@ class TrainConfig:
     energy_ground_truth = "./data/energy"
 
     audio_ground_truth = "./data/LJSpeech-1.1/wavs"
-    alignment_path = "./data/alignments"
-    data_path = './data/train.txt'
+    alignment_path = "./data/mfa_alignments"
+    data_path = './data/train_phones.txt'
 
     wandb_project = 'fastspeech_example'
 
