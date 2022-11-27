a49d3fa54e1d35d8074f325e52b8488364c7a206
Михаил Олейник <mikhailoleynik@192.168.1.3>
2022-11-27 21:56:03 +0300

    add MFA

--- bins/setup.sh
+++ bins/setup.sh
@@ -2,17 +2,23 @@
 
 mkdir ./data
 mkdir ./checkpoints
-# wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o /dev/null
-# tar -xvf LJSpeech-1.1.tar.bz2 >> /dev/null
-# mv LJSpeech-1.1 ./data/LJSpeech-1.1
-# rm LJSpeech-1.1.tar.bz2
+wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o /dev/null
+tar -xvf LJSpeech-1.1.tar.bz2 >> /dev/null
+mv LJSpeech-1.1 ./data/LJSpeech-1.1
+rm LJSpeech-1.1.tar.bz2
 
 gdown https://drive.google.com/u/0/uc?id=1-EdH0t0loc6vPiuVtXdhsDtzygWNSNZx
 mv train.txt ./data/
 
+gdown https://drive.google.com/u/0/uc?id=1NJfQEq-NNgat0AbCa92umHgDTkY1yQwT
+mv train_phones ./data/
+
+gdown https://drive.google.com/u/0/uc?id=1KyOdopKxOG3q4zYJg1aze8ujWc9bwiwW
+mv mfa_alignments ./data/
+
 #download Waveglow
-gdown https://drive.google.com/u/0/uc?id=1WsibBTsuRg_SF2Z6L6NFRTT-NjEy1oTx
-mv waveglow_256channels_ljs_v2.pt ..vocoder/waveglow/pretrained_model/waveglow_256channels.pt
+# gdown https://drive.google.com/u/0/uc?id=1WsibBTsuRg_SF2Z6L6NFRTT-NjEy1oTx
+# mv waveglow_256channels_ljs_v2.pt ..vocoder/waveglow/pretrained_model/waveglow_256channels.pt
 
 # load pre-computed melspecs, pitch and energy vectors
 gdown https://drive.google.com/u/0/uc?id=1lwes4q0G_KSw9V4kKf7YNDTJMDnwSw3t
@@ -22,8 +28,6 @@ mv melspecs ./data
 gdown https://drive.google.com/u/0/uc?id=1GxcW3Zjp9gWeTEbaZ0mb-QfObSqHzkZ4
 tar -xvf pitch.tar.gz
 mv pitch ./data
-
-
 
 gdown https://drive.google.com/u/0/uc?id=1pgeQcBnDNZlND3lLXlIJ1bdrOaN_T7ZL
 tar -xvf energy.tar.gz
